a855f0dd9936ab5b51d5efc08a31bf27773569b0
Wellerson <wllrsn.c@gmail.com>
2023-09-03 12:00:31 -0300

    feat: added login screen imgs

---
+++ packages/mdx/theme.js
@@ -0,0 +1,16 @@
+import { themes } from "mdx-deck";
+
+const { future } = themes;
+
+export default {
+  ...future,
+  colors: {
+    text: '#c6d1dd',
+    background: "#272425",
+  },
+  // Customize your presentation theme here.
+  //
+  // Read the docs for more info:
+  // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
+  // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
+};
